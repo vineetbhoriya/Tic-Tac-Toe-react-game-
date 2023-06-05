1295738548be4c6ae9da572cbbc5eec1c4de9a4a
Vineet Bhoriya <105233380+vineetbhoriya@users.noreply.github.com>
2023-06-05 09:36:56 +0530

    done

--- src/Components/Buttons.jsx
+++ src/Components/Buttons.jsx
@@ -2,26 +2,3 @@ import React from "react";
 
 import Square from "./Square";
 
-export default function Buttons() {
-  return (
-    <div>
-      <div>
-        <div className="board-row">
-          <Square></Square>
-          <Square></Square>
-          <Square></Square>
-        </div>
-        <div className="board-row">
-          <Square></Square>
-          <Square></Square>
-          <Square></Square>
-        </div>
-        <div className="board-row">
-          <Square></Square>
-          <Square></Square>
-          <Square></Square>
-        </div>
-      </div>
-    </div>
-  );
-}
